7b34681eff7ff0923b2b80c112d499d612cd38a3
guilhermealcar <guiialcar@gmail.com>
2023-11-10 11:19:33 +0100

    Optimized coordinateToStreet.py script

--- coordinateToStreet.py
+++ coordinateToStreet.py
@@ -25,24 +25,7 @@ def open_google_maps(coordinates):
     url = f"https://www.google.com/maps/place/{latitude},{longitude}"
     
     webbrowser.open(url)
-
-def get_google_maps_link(coordinates):
-    """
-    Generates a Google Maps link with the specified coordinates.
-
-    Parameters:
-    - coordinates (tuple): A tuple containing latitude and longitude.
-
-    Returns:
-    - str: Google Maps link for the specified coordinates.
-    """
-    if len(coordinates) != 2:
-        raise ValueError("Coordinates should be a tuple containing latitude and longitude.")
-
-    latitude, longitude = coordinates
-    url = f"https://www.google.com/maps/place/{latitude},{longitude}"
-
-    return url
+    return(url)
 
 latitude = 48.76385  # Example latitude
 longitude = 11.42065  # Example longitude
@@ -53,7 +36,8 @@ print(f"Street Name: {street_name}")
 
 # Example usage - open google maps
 coordinates = (latitude, longitude)
-open_google_maps(coordinates)
 
-google_maps_link = get_google_maps_link(coordinates)
+
+# Example usage - get the link
+google_maps_link = open_google_maps(coordinates)
 print(google_maps_link)
