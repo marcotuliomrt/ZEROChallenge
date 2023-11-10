28196edf6d5591618feed44d6407b73438a546e9
Adneycm <adneycm@al.insper.edu.br>
2023-11-10 23:23:22 +0100

    django visualization

--- zeroChallenge/signs/utils.py
+++ zeroChallenge/signs/utils.py
@@ -1,35 +1,49 @@
 from geopy.geocoders import Nominatim
+import folium
 from folium.vector_layers import Marker
 import random
 
-def generate_random_data(num_points):
+def generate_random_coordinates(num_points):
+    file_path = 'signs/random_coordinates_data.txt'
     coordinates = []
-    for n in range(num_points):
-        latitude = random.uniform(48.7, 48.8)  # Adjust latitude range as needed
-        longitude = random.uniform(11.3, 11.5)  # Adjust longitude range as needed
-        coordinates.append((latitude, longitude))
+    with open(file_path, 'r') as file:
+        for line in file:
+            coordinates.append(line.strip().split(','))  
     return coordinates
 
-def get_address(coordinates):
-  """
-  Get the address given the latitude and longitude parameters
-  """
-  latitude, longitude = coordinates
-  geolocator = Nominatim(user_agent="geo_locator")
-  location = geolocator.reverse((latitude, longitude), language="en")
+def generate_random_classifications(num_points):
+    classifications = []
+    for n in range(num_points):
+        p = random.choice(['Tree', 'Dirt', 'Sticker'])
+        classifications.append(random.choice([p, 'Good']))
+    return classifications
 
-  if location:
-    address_parts = [
-        location.raw.get('address', {}).get('city'),
-        location.raw.get('address', {}).get('postcode'),
-        location.raw.get('address', {}).get('country')
-    ]
-    address_filtered = ", ".join(filter(None, address_parts))
-    return address_filtered
-  else:
-    return "Address not found"
+def get_address():
+    """
+    Get the address given the latitude and longitude parameters
+    """
+    file_path = 'signs/random_adress_data.txt'
+    adresses = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            adresses.append(line.strip())  
+    return adresses
+
+  
+#   geolocator = Nominatim(user_agent="geo_locator")
+#   location = geolocator.reverse((latitude, longitude), language="en")
+
+#   if location:
+#     address_parts = [
+#         location.raw.get('address', {}).get('city'),
+#         location.raw.get('address', {}).get('postcode')
+#     ]
+#     address_filtered = ", ".join(filter(None, address_parts))
+#     return address_filtered
+#   else:
+#     return "Address not found"
     
-def create_colored_marker(classification, coordinates, color):
+def create_marker(classification, coordinates, color):
   latitude, longitude = coordinates
   address_filtered = get_address(latitude, longitude)
   popup_text = f"{classification} | {address_filtered}"
--- zeroChallenge/signs/views.py
+++ zeroChallenge/signs/views.py
@@ -2,65 +2,81 @@ from django.shortcuts import render, redirect
 from .models import Sign
 from django.views.decorators.csrf import csrf_exempt
 import folium
-from .utils import generate_random_data, get_address, create_colored_marker
+from .utils import \
+    generate_random_coordinates, \
+    generate_random_classifications, create_marker, get_address
 
 
 
 @csrf_exempt
 def index(request):
-    num_points = 20
-    coordinates = generate_random_data(num_points)
+    all_signs = Sign.objects.all()
 
-    latitude, longitude = 48.76385, 11.42065
+    initial_latitude, initial_longitude = 48.765633, 11.423611 # Center of ingolstadt
 
-    marker_latitude, marker_longitude = latitude, longitude
-
-    my_map = folium.Map(location=[latitude, longitude], zoom_start=12, width='800px', height='800px')
+    my_map = folium.Map(location=[initial_latitude, initial_longitude], zoom_start=15, width='800px', height='800px')
 
     # Add a marker to the map
-    folium.Marker(location=[marker_latitude, marker_longitude], popup='Marker').add_to(my_map)
+    for sign in all_signs:
+        classification = sign.classification
+        adress = sign.adress
+        latitude = float(sign.latitude)
+        longitude = float(sign.longitude)
+        color = 'green' if classification == 'Good' else 'red'
+        
+        popup_text = f"{classification} | {adress}"
+        folium.Marker(location=[latitude, longitude], icon=folium.Icon(color=color), popup=folium.Popup(popup_text, parse_html=True)).add_to(my_map)
 
     # Convert the Folium map to HTML
     map_html = my_map._repr_html_()
-
-
-    all_signs = Sign.objects.all()
 
     return render(request, 'signs/index.html', {'signs': all_signs, 'map_html': map_html})
         
 def solved(request):
 
-    latitude, longitude = 48.76385, 11.42065
+    signs = Sign.objects.filter(solved=True)
 
-    marker_latitude, marker_longitude = latitude, longitude
+    initial_latitude, initial_longitude = 48.765633, 11.423611 # Center of ingolstadt
 
-    my_map = folium.Map(location=[latitude, longitude], zoom_start=12, width='800px', height='800px')
+    my_map = folium.Map(location=[initial_latitude, initial_longitude], zoom_start=15, width='800px', height='800px')
 
     # Add a marker to the map
-    folium.Marker(location=[marker_latitude, marker_longitude], popup='Marker').add_to(my_map)
+    for sign in signs:
+        classification = sign.classification
+        adress = sign.adress
+        latitude = float(sign.latitude)
+        longitude = float(sign.longitude)
+        color = 'green' if classification == 'Good' else 'red'
+        
+        popup_text = f"{classification} | {adress}"
+        folium.Marker(location=[latitude, longitude], icon=folium.Icon(color=color), popup=folium.Popup(popup_text, parse_html=True)).add_to(my_map)
 
     # Convert the Folium map to HTML
     map_html = my_map._repr_html_()
-
-    signs = Sign.objects.filter(solved = 'True')
 
     return render(request, f'signs/solved.html', {'signs': signs, 'map_html': map_html})
 
 def unsolved(request):
 
-    latitude, longitude = 48.76385, 11.42065
+    signs = Sign.objects.filter(solved=False)
 
-    marker_latitude, marker_longitude = latitude, longitude
+    initial_latitude, initial_longitude = 48.765633, 11.423611 # Center of ingolstadt
 
-    my_map = folium.Map(location=[latitude, longitude], zoom_start=12, width='800px', height='800px')
+    my_map = folium.Map(location=[initial_latitude, initial_longitude], zoom_start=15, width='800px', height='800px')
 
     # Add a marker to the map
-    folium.Marker(location=[marker_latitude, marker_longitude], popup='Marker').add_to(my_map)
+    for sign in signs:
+        classification = sign.classification
+        adress = sign.adress
+        latitude = float(sign.latitude)
+        longitude = float(sign.longitude)
+        color = 'green' if classification == 'Good' else 'red'
+        
+        popup_text = f"{classification} | {adress}"
+        folium.Marker(location=[latitude, longitude], icon=folium.Icon(color=color), popup=folium.Popup(popup_text, parse_html=True)).add_to(my_map)
 
     # Convert the Folium map to HTML
     map_html = my_map._repr_html_()
-
-    signs = Sign.objects.filter(solved = 'False')
 
     return render(request, f'signs/unsolved.html', {'signs': signs, 'map_html': map_html})
 
